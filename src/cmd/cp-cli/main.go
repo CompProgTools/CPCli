856f98dbb1cd586cb2b8ea0bc4548b29eb084042
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2025-12-23 17:56:36 -0500

    chore: updated main file with go version

--- src/cmd/cp-cli/main.go
+++ src/cmd/cp-cli/main.go
@@ -1,2 +1,166 @@
-package cpcli
+package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/CompProgTools/CPCli/src/config"
+	"github.com/CompProgTools/CPCli/src/internal/subcommands"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/erikgeiser/promptkit/selection"
+	"github.com/erikgeiser/promptkit/textinput"
+	"github.com/pkg/browser"
+)
+
+var (
+	titleStyle = lipgloss.NewStyle().
+		Bold(true).
+		Foreground(lipgloss.Color("12")).
+		MarginBottom(1)
+
+	successStyle = lipgloss.NewStyle().
+		Foreground(lipgloss.Color("10"))
+
+	errorStyle = lipgloss.NewStyle().
+		Foreground(lipgloss.Color("9"))
+
+	yellowStyle = lipgloss.NewStyle().
+		Foreground(lipgloss.Color("11"))
+)
+
+func linkAccount() error {
+	platforms := []string{"Codeforces", "LeetCode", "Back"}
+
+	sp := selection.New("Select a platform to link: ", platforms)
+	sp.Filter = nil
+
+	platform, err := sp.RunPrompt()
+	if err != nil {
+		return err
+	}
+
+	if platform == "Back" {
+		return nil
+	}
+
+	for {
+		input := textinput.New(fmt.Sprintf("Enter your %s username: ", platform))
+		input.Placeholder = "username"
+
+		handle, err := input.RunPrompt()
+		if err != nil {
+			return err
+		}
+
+		valid := false
+		if platform == "Codeforces" {
+			valid, err = subcommands.ValidateCodeforcesUser(handle)
+		} else if platform == "LeetCode" {
+			valid, err = subcommands.ValidateCodeforcesUser(handle)
+		}
+
+		if err != nil {
+			fmt.Println(errorStyle.Render(fmt.Sprintf("Error connecting to %s: %v", platform, err)))
+		}
+
+		if !valid {
+			fmt.Println(errorStyle.Render(fmt.Sprintf("User not found on %s. Try again.", platform)))
+			continue
+		}
+
+		break
+	}
+
+	fmt.Println(successStyle.Render(fmt.Sprintf("%s account linked successfully!", platform)))
+
+	return nil
+}
+
+func showMenu() error {
+	allLinked, err := config.IsAllLinked()
+	if err != nil {
+		return err
+	}
+
+	options := []string{"View repository"}
+	if !allLinked {
+		options = append(options, "Link Account")
+	}
+	options = append(options, "Coming soon...", "Exit")
+
+	fmt.Println(titleStyle.Render("Hi! This is CPCli, a command line interface for competitive programmers!"))
+
+	sp := selection.New("What would you like to do?", options)
+	sp.Filter = nil
+
+	choice, err := sp.RunPrompt()
+	if err != nil {
+		return err
+	}
+
+	switch choice {
+	case "View repository":
+		fmt.Println(successStyle.Render("Opening CPCli GitHub repository"))
+		_ = browser.OpenURL("https://github.com/CompProgTools/CPCli")
+	case "Link Account":
+		return linkAccount()
+	case "Coming soon...":
+		fmt.Println(yellowStyle.Render("Stay tuned!"))
+	case "Exit":
+		fmt.Println(errorStyle.Render("Goodbye!"))
+	}
+
+	return nil
+}
+
+func main() {
+	if len(os.Args > 1) {
+		subcommand := os.Args[1]
+		args := os.Args[2:]
+
+		var err error 
+		switch subcommand {
+		case "sync":
+			err = subcommands.RunSync()
+		case "streak":
+			err = subcommands.RunStreak()
+		case "stats":
+			err = subcommands.RunStats()
+		case "graph":
+			err = subcommands.RunGraph()
+		case "test":
+			err = subcommands.RunTest()
+		case "config":
+			p := tea.NewProgram(subcommands.NewConfigModel())
+			if _, err := p.Run(); err != nil {
+				fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+				os.Exit(1)
+			}
+			return
+		case "template":
+			err = subcommands.RunTemplate(args)
+		case "daily":
+			err = subcommands.RunDaily()
+		case "openKat":
+			err = subcommands.RunOpenKat(args)
+		case "cf":
+			err = subcommands.RunCF(args)
+		case "update":
+			err = subcommands.RunUpdate(args)
+		default:
+			fmt.Fprintf(os.Stderr, errorStyle.Render(fmt.Sprintf("Unknown command: %s\n", subcommand)))
+			os.Exit(1)
+		}
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, errorStyle.Render(fmt.Sprintf("Error: %v\n", err)))
+			os.Exit(1)
+		}
+	} else {
+		if err := showMenu; err != nil {
+			fmt.Fprintf(os.Stderr, "Error %v\n", err)
+			os.Exit(1)
+		}
+	}
+}
--- src/config/handler.go
+++ src/config/handler.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/CompProgTools/src/internal/models"
+	"github.com/CompProgTools/CPCli/src/internal/models"
 )
 
 var (
